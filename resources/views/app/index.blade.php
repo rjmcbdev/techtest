a6fa4db8a180128d0dac50703a82131e49de70f1
rjmcbdev <rjmcbdev@gmail.com>
2020-03-25 03:50:44 +0800

    added app controller and views with bootstrap template

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,24 @@
+@extends("layouts.layout")
+
+@section("content")
+
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Month</th>
+            <th>Number of Cases</th>
+            <th>Forecasted Cost</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>App</title>
+  <!-- Bootstrap core CSS -->
+    <link href="{{ url('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+
+  </head>
+
+  <body>
+
+    <header>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <strong>Forecast App</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <main role="main">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 py-5">
+              @yield("content")
+            </div>
+        </div>
+    </main>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="{{ url('/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+  </body>
+</html>
