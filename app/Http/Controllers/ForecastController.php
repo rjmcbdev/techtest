95e115bd1ef3348861b832c14b4733d30da44ad7
rjmcbdev <rjmcbdev@gmail.com>
2020-03-25 12:05:23 +0800

    added validation in forecast controller

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -12,6 +12,44 @@ class ForecastController extends Controller
 
     function getForecast(Request $request){
 
+
+        /* Validation */
+        if(!is_numeric($request["no-of-studies"])){
+            $data["result"] = array(
+                "flag" => false,
+                "message" => "No of Studies must be a number!",
+                "class" => "warning"
+            );
+            return response($data);
+        }
+        if(!is_numeric($request["growth-per-month"])){
+            $data["result"] = array(
+                "flag" => false,
+                "message" => "Growth per month must be a number!",
+                "class" => "warning"
+            );
+            return response($data);
+        }
+
+        if(!is_numeric($request["months-to-forecast"])){
+            $data["result"] = array(
+                "flag" => false,
+                "message" => "Months to forecast must be a number!",
+                "class" => "warning"
+            );
+            return response($data);
+        }
+
+        if(empty($request["no-of-studies"])|| empty($request["growth-per-month"]) || empty($request["months-to-forecast"])){
+            $data["result"] = array(
+                "flag" => false,
+                "message" => "Entries must not be empty!",
+                "class" => "warning"
+            );
+            return response($data);
+        }
+        /* End Validation */
+
         $studies = new Studies();
         $studies->setNoOfStudies($request["no-of-studies"])
         ->setGrowthPerMonth($request["growth-per-month"]);//in percent
