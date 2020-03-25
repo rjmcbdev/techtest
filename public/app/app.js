bc4ec36888f33e62e4f0604f776b23685b6fe7dd
rjmcbdev <rjmcbdev@gmail.com>
2020-03-25 12:31:20 +0800

    added validation alerts and waitme

--- public/app/app.js
+++ public/app/app.js
@@ -1,21 +1,25 @@
+
+
 $(function(){
 
+    /* init datatable */
+    var forecastDTable = $('#forecast-table').DataTable( {
+        dom: 'Bfrtip',
+        columnDefs : [{"targets":0, "type":"date"}],
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'pdfHtml5'
+        ]
+    } );
+
+    /* init btn modal show */
     $("#btn-check-forecast").click(function(){
         $("#forecast-modal").modal("show");
     });
-
+     /* init btn forecast */
     $("#btn-get-forecast").click(function(){
-
-        // var person = {
-        //     firstName: "Christophe",
-        //     lastName: "Coenraets",
-        //     blogURL: '<a class="vglnk" href="http://coenraets.org" rel="nofollow"><span>http</span><span>://</span><span>coenraets</span><span>.</span><span>org</span></a>'
-        // };
-
-        // var html = Mustache.to_html(template, person);
-
-
-
+        $("#forecast-modal").modal("hide");
         formData = $("#forecast-form").serializeArray();
         $.ajax({
             dataType : "json",
@@ -24,18 +28,46 @@ $(function(){
             data : formData
             ,beforeSend : function(){
 
+                $("#forecast-table tbody").html("");
+                $("#forecast-table-container").waitMe();
+
             },success : function(data){
+                console.log(data);
+                $("#forecast-table-container").waitMe("hide");
+
+
+
+                if(data.result.flag== false){
+                    Swal.fire("Warning",data.result.message,data.result.class);
+                    return false;
+                }
+
+
+
+
+                forecastDTable.destroy();
+                /* tbody template */
                 var template = "";
                     template += "{{#result}}";
                     template += "<tr>";
                     template += "<td>{{month}}</td>";
-                    template += "<td>{{days}}</td>";
                     template += "<td class='text-right'>{{noOfStudiesFormatted}}</td>";
                     template += "<td class='text-right'>{{monthlyCostFormatted}}</td>";
                     template += "</tr>";
                     template += "{{/result}}";
                 var html = Mustache.to_html(template, data);
-                $("#forecast-table tbody").html(html);
+                  $("#forecast-table tbody").html(html);
+
+    /*  Re-init datatable  */
+    forecastDTable = $('#forecast-table').DataTable( {
+        dom: 'Bfrtip',
+        columnDefs : [{"targets":0, "type":"date"}],
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'pdfHtml5'
+        ]
+    } );
 
 
             }
